0f7768a560eb51f34cf507757aabe4b60fdc0be6
Martin Fuska <martin.fuska@gmail.com>
2013-10-08 16:51:45 +0200

    fixed errors for compiler

--- src/android/GeoListener.java
+++ src/android/GeoListener.java
@@ -1,5 +1,7 @@
 package org.apache.cordova.tickk;
 
+import android.location.Location;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -7,7 +9,12 @@ import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 
+import android.util.Log;
 
 public class GeoListener implements LocationListener {
+
+    public void onLocationChanged(Location location) {
+        Log.d("[Cordova GeoService]", "GeoListener - onLocationChanged");
+    }
     
 }

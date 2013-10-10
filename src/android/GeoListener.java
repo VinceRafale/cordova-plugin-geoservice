36cdab7137de2932df0174708c0fe3ef3233a6f0
Martin Fuska <martin.fuska@gmail.com>
2013-10-10 11:51:58 +0200

    modified as first working basic setup to periodically update location

--- src/android/GeoListener.java
+++ src/android/GeoListener.java
@@ -10,11 +10,91 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 
 import android.util.Log;
+import android.content.Context;
+import android.os.Bundle;
 
-public class GeoListener implements LocationListener {
+public class GeoListener implements 
+    LocationListener,
+    GooglePlayServicesClient.ConnectionCallbacks,
+    GooglePlayServicesClient.OnConnectionFailedListener {
 
+    // reference to parent
+    private GeoService owner;
+
+    // A request to connect to Location Services
+    private LocationRequest mLocationRequest;
+
+    // Stores the current instantiation of the location client in this object
+    private LocationClient mLocationClient;
+
+    // Global variable to hold the current location
+    private Location mCurrentLocation;
+
+    public GeoListener(GeoService service) {
+        this.owner = service;
+    }
+
+    /**
+     * Report location
+     *
+     * @param location The updated location.
+     */
+    @Override
     public void onLocationChanged(Location location) {
-        Log.d("[Cordova GeoService]", "GeoListener - onLocationChanged");
+        Log.d("[Cordova GeoService]", "GeoListener - location has been updated");
+
+        Log.d("[Cordova GeoService]", "GeoListener - location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude()));
+
+        this.owner.win(location);
+    }
+
+    /*
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d("[Cordova GeoService]", "GeoListener - connected");
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+
+    /*
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+    @Override
+    public void onDisconnected() {
+        Log.d("[Cordova GeoService]", "GeoListener - disconnected");
+    }
+
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d("[Cordova GeoService]", "GeoListener - connection failed");
     }
     
+    public void get() {
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create();
+        // Use high accuracy
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Set the update interval to 5 seconds
+        mLocationRequest.setInterval(5000);
+        // Set the fastest update interval to 1 second
+        mLocationRequest.setFastestInterval(1000);
+
+
+        Context context = this.owner.cordova.getActivity().getApplicationContext();
+        mLocationClient = new LocationClient(context, this, this);
+
+        mLocationClient.connect();
+
+    }
 }
--- src/android/GeoService.java
+++ src/android/GeoService.java
@@ -1,16 +1,21 @@
 package org.apache.cordova.tickk;
 
+import android.location.Location;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.util.Log;
 
 
 public class GeoService extends CordovaPlugin {
+
+    private GeoListener geoListener;
 
     /**
      * Constructor.
@@ -22,9 +27,38 @@ public class GeoService extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         Log.d("[Cordova GeoService]", "execute: " + action);
 
+        if (this.geoListener == null) {
+            this.geoListener = new GeoListener(this);
+
+            if (action.equals("get")) {
+                this.geoListener.get();
+            }
+        }
 
 
         return true;
+    }
+
+    public void win(Location location) {
+        Log.d("[Cordova GeoService]", "win");
+        JSONObject loc = new JSONObject();
+
+        try {
+            loc.put("latitude", location.getLatitude());
+            loc.put("longitude", location.getLongitude());
+            loc.put("altitude", (location.hasAltitude() ? location.getAltitude() : null));
+            loc.put("accuracy", location.getAccuracy());
+            loc.put("heading", (location.hasBearing() ? (location.hasSpeed() ? location.getBearing() : null) : null));
+            loc.put("velocity", location.getSpeed());
+            loc.put("timestamp", location.getTime());
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            Log.d("[Cordova GeoService]", "win - exception");
+            e.printStackTrace();
+        }
+
+        
+        PluginResult result = new PluginResult(PluginResult.Status.OK, loc);
     }
 
 }
